cb7570cb24b399b24de9121a9b04cc7d323afea2
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-24 11:57:56 +0530

    new

--- deals/models.py
+++ deals/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User = get_user_model()
 
 
 class Deal(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    age = models.IntegerField(default=0)
+    contact_person = models.CharField(max_length=100)
+    organization = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    value = models.IntegerField(default=0)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+
+class Agent(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
