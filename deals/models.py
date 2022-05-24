be0a1e7773e40791b7f1527d96dda85c4cbb72a9
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-24 11:19:26 +0530

    new

--- deals/models.py
+++ deals/models.py
@@ -6,3 +6,4 @@ from django.db import models
 class Deal(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
