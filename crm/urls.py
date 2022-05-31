f6c143d84907279b66b85d186acc39fd3fa759ca
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-31 17:24:32 +0530

    new

--- crm/urls.py
+++ crm/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from deals.views import LandingPageView
 
@@ -11,6 +11,7 @@ urlpatterns = [
     path('', LandingPageView.as_view(), name='landing-page'),
     path('deals/', include('deals.urls', namespace="deals")),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
