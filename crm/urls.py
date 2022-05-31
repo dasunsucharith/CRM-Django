96fc8bc1dce2c0974a138556d42903a71fb53bf2
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-31 15:55:41 +0530

    new

--- crm/urls.py
+++ crm/urls.py
@@ -1,9 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from deals.views import LandingPageView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing-page'),
-    path('deals/', include('deals.urls', namespace="deals"))
+    path('deals/', include('deals.urls', namespace="deals")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
