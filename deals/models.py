a183bc3ecf7c945015ebb52dd92bee73c44b4173
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-06-01 18:08:31 +0530

    Update models.py

--- deals/models.py
+++ deals/models.py
@@ -1,16 +1,20 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_save
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class User(AbstractUser):
-    pass 
+    pass
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
+
 
 class Deal(models.Model):
     contact_person = models.CharField(max_length=100)
@@ -24,9 +28,18 @@ class Deal(models.Model):
     def __str__(self):
         return f"{self.contact_person}"
 
+
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.email
+
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(post_user_created_signal, sender=User)
